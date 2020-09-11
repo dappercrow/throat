8719ec60df028b0f762af560dd56285efdf7fd45
dappercrow <70492714+dappercrow@users.noreply.github.com>
2020-09-11 11:43:21 -0700

    Add config option to manage which levels users can PM others at

--- app/config.py
+++ app/config.py
@@ -36,6 +36,9 @@ cfg_defaults = {  # key => default value
             "edit_history": False,
             "anonymous_modding": False,
 
+            "send_pm_to_admin_min_level": 0,
+            "send_pm_to_user_min_level": 3,
+
             "daily_sub_posting_limit": 10,
             "daily_site_posting_limit": 25,
 
